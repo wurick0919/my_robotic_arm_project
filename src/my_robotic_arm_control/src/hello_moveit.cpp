f15644ff73aa722fe5e4f965ac48048e5019a9ec
Rick Wu <wurick0919@gmail.com>
2026-01-20 15:01:56 +0000

    add control pkg, use cpp api to talk to moveit2

--- src/my_robotic_arm_control/src/hello_moveit.cpp
+++ src/my_robotic_arm_control/src/hello_moveit.cpp
@@ -3,8 +3,8 @@
 
 #include <rclcpp/rclcpp.hpp>
 #include <moveit/move_group_interface/move_group_interface.hpp>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
-#include <tf2/LinearMath/Quaternion.hpp>
+// #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+// #include <tf2/LinearMath/Quaternion.hpp>
 
 
 int main(int argc, char * argv[])
@@ -16,6 +16,44 @@ int main(int argc, char * argv[])
   );
 
   auto const logger = rclcpp::get_logger("hello_moveit");
+
+  // main code goes here
+  // Create the MoveIt MoveGroup Interface
+  using moveit::planning_interface::MoveGroupInterface;
+  auto move_group_interface = MoveGroupInterface(node, "arm"); // "" is the name of the group of joints we are controlling
+  
+
+  std::vector<double> joint_group_positions = {0.1, -0.8, -0.8}; 
+  move_group_interface.setJointValueTarget(joint_group_positions);
+
+
+  // // Set a target Pose
+  // auto const target_pose = []{
+  //   geometry_msgs::msg::Pose msg;
+  //   msg.orientation.w = 1.0;
+  //   msg.position.x = 0.1;
+  //   msg.position.y = 0.1;
+  //   msg.position.z = 0.1;
+  //   return msg;
+  // }();
+  // move_group_interface.setPoseTarget(target_pose);
+  // move_group_interface.setPositionTarget(-0.28, -0.3, 0.39);
+
+
+  // Create a plan to that target pose
+  auto const [success, plan] = [&move_group_interface]{
+    moveit::planning_interface::MoveGroupInterface::Plan msg;
+    auto const ok = static_cast<bool>(move_group_interface.plan(msg));
+    return std::make_pair(ok, msg);
+  }();
+
+  // Execute the plan
+  if(success) {
+    move_group_interface.execute(plan);
+  } else {
+    RCLCPP_ERROR(logger, "Planning failed!");
+  }
+
   rclcpp::shutdown();
   return 0;
 }
--- src/my_robotic_arm_moveit_config/launch/demo.launch.py
+++ src/my_robotic_arm_moveit_config/launch/demo.launch.py
@@ -1,23 +1,7 @@
-# from moveit_configs_utils import MoveItConfigsBuilder
-# from moveit_configs_utils.launches import generate_demo_launch
-
-
-# def generate_launch_description():
-#     moveit_config = MoveItConfigsBuilder("my_robot", package_name="my_robotic_arm_moveit_config").to_moveit_configs()
-#     return generate_demo_launch(moveit_config)
-
-
 from moveit_configs_utils import MoveItConfigsBuilder
 from moveit_configs_utils.launches import generate_demo_launch
-import os
+
 
 def generate_launch_description():
-    # Explicitly define where the controllers file is
-    moveit_config = (
-        MoveItConfigsBuilder("my_robot", package_name="my_robotic_arm_moveit_config")
-        .robot_description(file_path="config/my_robot.urdf.xacro") # Or your .urdf
-        .trajectory_execution(file_path="config/moveit_controllers.yaml") # FORCE THIS
-        .to_moveit_configs()
-    )
-    
+    moveit_config = MoveItConfigsBuilder("my_robot", package_name="my_robotic_arm_moveit_config").to_moveit_configs()
     return generate_demo_launch(moveit_config)
